82c806710a0ecc44c174763ca49477e7daf01a26
Mahima <mahimapatel54@gmail.com>
2021-03-19 21:48:54 +0530

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :blogs
+  # resources encapsulates all the popular routes that applications use
+  # it contains all types of routes not just one. 
+  # like blogs/:id/edit etc
+  # check using rails routes command in terminal
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
