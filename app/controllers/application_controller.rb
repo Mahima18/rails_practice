30a9b5d9d6b1a4eb6f7da82111062b851e63bac1
Mahima <mahimapatel54@gmail.com>
2021-03-30 16:22:55 +0530

    implemented rubygem but failed so hard coded the copyright info in here

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,18 @@ class ApplicationController < ActionController::Base
 	include SetSource
 	include CurrentUserConcern
 	include DefaultPageContent
+
+	before_action :set_copyright
+
+	def set_copyright
+		@copyright = MinervaViewTool::Renderer.copyright 'Mahima P', 'All rights reserved'
+	end
+end
+
+module MinervaViewTool
+	class Renderer
+		def self.copyright name,msg
+			"&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+		end 	
+	end
 end
